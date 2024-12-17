6928437f4df9885556a084549f25339f8c259548
adarsh <adarsh@sap.com>
2024-12-18 01:50:24 +0530

    deploy to render

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ var mongoose = require("mongoose");
 var methodOverride = require("method-override");
 var expressSanitizer = require("express-sanitizer");
 
-mongoose.connect("mongodb://localhost/restfulBlogApp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/restfulBlogApp", { useNewUrlParser: true });
+const dbPassword = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://ouadarsh:${dbPassword}@cluster0.lgoyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+async function run() {
+try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+}
+}
+run().catch(console.dir);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
